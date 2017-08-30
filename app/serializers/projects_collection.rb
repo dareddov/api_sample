0011654c78a37a70ef646f35caf6426e4b401b35
Marek Caputa <me@mark97.net>
2017-08-31 00:12:09 +0200

    Add sorting for projects by name, budget

--- app/serializers/projects_collection.rb
+++ app/serializers/projects_collection.rb
@@ -1,16 +1,40 @@
 class ProjectsCollection
+  SORTABLE_FIELDS = [:name, :budget].sort.freeze
+
   include Rails.application.routes.url_helpers
+  attr_reader :params
   attr_reader :projects
 
-  def initialize(projects)
-    @projects = projects
+  def initialize(params)
+    @params = params
+    @projects = params[:sort] ? Project.order(sort_params).page(params[:page]) : Project.page(params[:page])
   end
 
   def to_json(_options = {})
+    return bad_request if invalid_sortable_fields
+
     {
       data: data_projects,
       links: links
     }.to_json
+  end
+
+  def response_status
+    invalid_sortable_fields ? :bad_request : :ok
+  end
+
+  def bad_request
+    { title: 'Bad request', description: 'Not supported attribute by sorting!', status: '400' }.to_json
+  end
+
+  private
+
+  def invalid_sortable_fields
+    !((sort_params.keys.map(&:to_sym) + SORTABLE_FIELDS).uniq.sort == SORTABLE_FIELDS)
+  end
+
+  def sort_params
+    SortParams.sorted_fields(params[:sort])
   end
 
   def data_projects
@@ -26,11 +50,11 @@ class ProjectsCollection
 
   def links
     links = {
-      self: projects_url(page: projects.current_page)
+      self: projects_url(page: projects.current_page, sort: params[:sort])
     }
 
-    links[:next] = projects_url(page: projects.next_page) if projects.next_page
-    links[:previous] = projects_url(page: projects.prev_page) if projects.prev_page
+    links[:next] = projects_url(page: projects.next_page, sort: params[:sort]) if projects.next_page
+    links[:previous] = projects_url(page: projects.prev_page, sort: params[:sort]) if projects.prev_page
 
     links
   end
